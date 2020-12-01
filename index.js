ed113437c665abcf68502ae1f8f5bd768b655f18
Romaan <romaan567@hotmail.com>
2020-12-01 16:05:12 -0500

    POST to /playlist with date: yyyy-mm-dd to generate playlist

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const app = express();
 const SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config();
 
-var scopes = ['playlist-modify-public'],
-  redirectUri = process.env.REDIRECT_URI,
-  clientId = process.env.SPOTIFY_CLIENT_ID,
-  state = process.env.STATE;
-
 var credentials = {
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
@@ -32,24 +27,6 @@ spotifyApi.refreshAccessToken().then(
     console.log('Could not refresh access token', err);
   }
 );
-
-// var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
-// console.log(authorizeURL); // Uncomment to get new code
-
-// spotifyApi.authorizationCodeGrant(process.env.SPOTIFY_AUTH_CODE).then(
-//   function (data) {
-//     console.log('The token expires in ' + data.body['expires_in']);
-//     console.log('The access token is ' + data.body['access_token']);
-//     console.log('The refresh token is ' + data.body['refresh_token']);
-
-//     // Set the access token on the API object to use it in later calls
-//     spotifyApi.setAccessToken(data.body['access_token']);
-//     spotifyApi.setRefreshToken(data.body['refresh_token']);
-//   },
-//   function (err) {
-//     console.log('Something went wrong!', err);
-//   }
-// );
 
 app.use(
   bodyParser.urlencoded({
@@ -91,35 +68,54 @@ const scrapeHot100 = async (url) => {
   return list;
 };
 
-app.post('/playlist', async (req, res) => {
-  const url = `https://www.billboard.com/charts/hot-100/${req.body.date}`;
+const fillArray = async (tracks) => {
+  let list = [];
 
-  let scraped = await scrapeHot100(url);
+  let promiseArray = tracks.map(async ({ songName, artist }) => {
+    let track = await spotifyApi.searchTracks(
+      `track:${songName} artist:${artist}`
+    );
 
-  // spotifyApi
-  //   .createPlaylist(`Time machine - ${req.body.date}`, { public: true })
-  //   .then(
-  //     function (data) {
-  //       console.log('Created playlist!');
-  //     },
-  //     function (err) {
-  //       console.log('Something went wrong!', err);
-  //     }
-  //   );
-
-  spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar').then(
-    function (data) {
-      console.log(
-        'Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name',
-        data.body.tracks.items
-      );
-    },
-    function (err) {
-      console.log('Something went wrong!', err);
+    if (track != undefined) {
+      if (track.body.tracks.items.length > 0) {
+        list.push(`spotify:track:${track.body.tracks.items[0].id}`);
+      }
     }
-  );
+  });
 
-  res.send('Success!');
+  const results = await Promise.all(promiseArray);
+
+  return list;
+};
+
+app.post('/playlist', async (req, res) => {
+  try {
+    const url = `https://www.billboard.com/charts/hot-100/${req.body.date}`;
+    let scraped = await scrapeHot100(url);
+
+    let newPlaylist = await spotifyApi.createPlaylist(
+      `Time machine - ${req.body.date}`,
+      { public: true }
+    );
+
+    let listOfSongs = await fillArray(scraped);
+
+    console.log('list of songs', listOfSongs);
+
+    spotifyApi.addTracksToPlaylist(newPlaylist.body.id, listOfSongs).then(
+      function (data) {
+        console.log('Added tracks to playlist!');
+      },
+      function (err) {
+        console.log('Something went wrong!', err);
+      }
+    );
+
+    res.send('Success!');
+  } catch (e) {
+    console.log('Something went wrong...', e);
+    res.send('Error', e);
+  }
 });
 
 app.get('/callback', (req, res) => {
